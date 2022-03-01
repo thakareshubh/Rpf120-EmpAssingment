5861f3e97fcbc18aee3eaffb690dc8a613d45dba
Shubham Thakare <shubhamthakare032@gmail.com>
2022-03-01 09:44:40 +0530

    Added Employee Attendance

---
+++ Uc-1-presentAbsent/Uc-1-presentAbsent/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Uc_1_presentAbsent
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //variable initialization
+            int isPresent = 1;
+            Random random = new Random();
+            int check = random.Next(0, 1);
+
+            //conditional statment 
+            if (check == isPresent)
+            {
+                Console.WriteLine("Employee is present");
+            }
+            else
+            {
+                Console.WriteLine("Employee is not present");
+            }
+        }
+    }
+}
