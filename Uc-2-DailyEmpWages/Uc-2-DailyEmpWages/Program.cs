edfe8f393357b6b035232c3faa9f2d5c1d2b225d
Shubham Thakare <shubhamthakare032@gmail.com>
2022-03-01 09:49:21 +0530

    Added daily wages of employee

---
+++ Uc-2-DailyEmpWages/Uc-2-DailyEmpWages/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Uc_2_DailyEmpWages
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //variable initialization
+            int SalaryPerHr = 20;
+            int empHr;
+            int TotalWage;
+            int FullTime = 1;
+            Random random = new Random();
+            int check = random.Next(0, 1);
+
+            //conditional statment
+            if (check == FullTime)
+            {
+                empHr = 8;
+            }
+            else
+            {
+                empHr = 0;
+
+            }
+            //wages of employee
+            TotalWage = SalaryPerHr * empHr;
+            Console.WriteLine("DailyEmpWages Wages of Employee: " + TotalWage);
+
+        }
+    }
+}
