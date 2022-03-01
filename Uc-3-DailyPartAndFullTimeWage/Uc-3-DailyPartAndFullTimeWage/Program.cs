25428f05268e513840e3f49bf01f0df7168b6fea
Shubham Thakare <shubhamthakare032@gmail.com>
2022-03-01 09:58:31 +0530

    Added switch command in daily wages file

---
+++ Uc-3-DailyPartAndFullTimeWage/Uc-3-DailyPartAndFullTimeWage/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace Uc_3_DailyPartAndFullTimeWage
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //variable initialization
+            int SalaryPerHr = 20;
+            int empHr;
+            int TotalWage;
+            int FullTime = 2;
+            int PartTime = 1;
+            Random random = new Random();
+            int check = random.Next(0, 3);
+
+            //conditional statment
+            if (check == FullTime)
+            {
+                empHr = 8;
+            }
+            else if(check == PartTime)
+            {
+                empHr = 4;
+
+            }
+            else
+            {
+                empHr= 0;
+            }
+            //wages of employee
+            TotalWage = SalaryPerHr * empHr;
+            Console.WriteLine("DailyEmpWages Wages of Employee: " + TotalWage);
+
+        }
+    }
+}
---
+++ Uc-4-UseOfSwitchInDailyWagePartTimeFullTime/Uc-4-UseOfSwitchInDailyWagePartTimeFullTime/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace Uc_4_UseOfSwitchInDailyWagePartTimeFullTime
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //variable initialization
+            int SalaryPerHr = 20;
+            int empHr;
+            int TotalWage;  
+            Random random = new Random();
+            int check = random.Next(0, 3);
+
+            //Switch statment
+            switch (check) 
+            {
+                case 1:empHr = 8;
+                    break;
+
+                case 2:empHr = 4;
+                    break;
+
+                default:
+                    empHr = 0;
+                    break;
+                    
+             }
+            
+            //wages of employee
+            TotalWage = SalaryPerHr * empHr;
+            Console.WriteLine("DailyEmpWages Wages of Employee: " + TotalWage);
+
+        }
+    }
+}
